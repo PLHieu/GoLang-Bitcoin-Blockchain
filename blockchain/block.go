93b7e3ed73a6dea1b9d247de777ec44c10c4a411
Phan Long Hieu <hieuflong@gmail.com>
2021-09-29 20:25:24 +0700

    Refactor

---
+++ blockchain/block.go
@@ -0,0 +1,42 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type BlockChain struct {
+	Blocks []*Block
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{Blocks: []*Block{CreateGenesis()}}
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	lastBlock := chain.Blocks[len(chain.Blocks)-1]
+	newBlock := CreateBlock(data, lastBlock.Hash)
+	chain.Blocks = append(chain.Blocks, newBlock)
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) deriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{Hash: []byte{}, Data: []byte(data), PrevHash: prevHash}
+	block.deriveHash()
+	return block
+}
+
+func CreateGenesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
--- main.go
+++ main.go
@@ -1,55 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"myblockchain/blockchain"
 )
 
-type BlockChain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-func (b *Block) deriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{Hash: []byte{}, Data: []byte(data), PrevHash: prevHash}
-	block.deriveHash()
-	return block
-}
-
-func CreateGenesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{blocks: []*Block{CreateGenesis()}}
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	lastBlock := chain.blocks[len(chain.blocks)-1]
-	newBlock := CreateBlock(data, lastBlock.Hash)
-	chain.blocks = append(chain.blocks, newBlock)
-}
-
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Prev: %x\n", block.PrevHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
