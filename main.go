bf8d9aa4bfcf23f147ecc62d3cd0f1c1e0c361f2
PLHieu <hieuflong@gmail.com>
2021-10-12 15:54:33 +0700

    Modify gitinorge

--- main.go
+++ main.go
@@ -1,24 +1,92 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"myblockchain/blockchain"
+	"os"
+	"runtime"
 )
 
+type CommandLine struct{}
+
 func main() {
-	chain := blockchain.InitBlockChain()
+	defer os.Exit(0)
+	cli := CommandLine{}
+	cli.run()
+}
 
-	//chain.AddBlock("First Block after Genesis")
-	//chain.AddBlock("Second Block after Genesis")
-	//chain.AddBlock("Third Block after Genesis")
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println(" createblockchain -address ADDRESS creates a blockchain and sends genesis reward to address")
+	fmt.Println(" printchain - Prints the blocks in the chain")
+}
 
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+func (cli *CommandLine) printChain() {
+	chain := blockchain.LoadBlockChain()
+	defer chain.Database.Close()
 	iter := chain.Iterator()
-	block := iter.Next()
-	for block != nil {
-		fmt.Printf("Prev: %x\n", block.PrevHash)
-		fmt.Printf("Data: %s\n", block.Data)
+
+	for {
+		block := iter.Next()
+
+		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
 		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("Nonce: %d\n", block.Nonce)
-		block = iter.Next()
+		fmt.Println()
+
+		if block.PrevHash == nil {
+			break
+		}
+	}
+}
+
+func (cli *CommandLine) createBlockChain(address string) {
+	chain := blockchain.InitBlockChain(address)
+	chain.Database.Close()
+	fmt.Println("Finished!")
+}
+
+func (cli *CommandLine) run() {
+	cli.validateArgs()
+
+	createBlockchainCmd := flag.NewFlagSet("createblockchain", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	createBlockchainAddress := createBlockchainCmd.String("address", "", "The address to send genesis block reward to")
+
+	switch os.Args[1] {
+	case "createblockchain":
+		err := createBlockchainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+
+	if createBlockchainCmd.Parsed() {
+		if *createBlockchainAddress == "" {
+			createBlockchainCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.createBlockChain(*createBlockchainAddress)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
 	}
 }
